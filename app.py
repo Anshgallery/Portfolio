e4a9fe734a4fa562fee830069a2fc1944f484230
Anshgallery <nextrgenansh@gmail.com>
2026-08-04 12:50:07 +0530

    cicd

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def experence():
     return render_template("experence.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0" ,port=8080)
+    app.run(debug=True,host="0.0.0.0" , port=8080)
